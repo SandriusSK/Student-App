3d7464005df9b6ac3ad94b368def393b2a35e8eb
g00402163 <G00402163@atu.ie>
2022-10-28 16:37:37 +0100

    Students name and email

--- src/main/java/ie/atu/Class/StudentApp.java
+++ src/main/java/ie/atu/Class/StudentApp.java
@@ -1,4 +1,19 @@
 package ie.atu.Class;
 
+import java.util.Scanner;
+
 public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter student name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("You entered : " + name);
+
+        System.out.println("Please enter student email");
+        String email = input.nextLine();
+        System.out.println("You entered : " + email);
+
+
+    }
 }
