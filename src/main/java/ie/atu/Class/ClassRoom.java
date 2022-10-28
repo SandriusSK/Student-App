5a51264975eca839716a592a679bb8af02db3f35
g00402163 <G00402163@atu.ie>
2022-10-28 18:07:24 +0100

    Added second file name/email/course

--- src/main/java/ie/atu/Class/ClassRoom.java
+++ src/main/java/ie/atu/Class/ClassRoom.java
@@ -3,6 +3,19 @@ package ie.atu.Class;
 public class ClassRoom {
     private String name;
     private String email;
+    private String course;
+
+    public ClassRoom() {
+        this.name = 0;
+        this.email = 0;
+        this.course = 0;
+    }
+
+    public ClassRoom(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
 
     public String getName() {
         return name;
@@ -18,6 +31,14 @@ public class ClassRoom {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
     }
 
 }
